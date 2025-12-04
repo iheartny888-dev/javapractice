81be6db44aab1c2094f165dffab373894c99b31c
iheartny888-dev <iheartny888@gmail.com>
2025-12-04 14:34:50 +0000

    a

--- Math1.java
+++ Math1.java
@@ -1,29 +1,34 @@
 class Calculator {
     //write functions for addition, substraction, multiplication, division, etc.
     
-    add() {
-
+    int add(int a, int b) {
+        return a + b;
     }
 
-    subtract() {
-
+    int subtract(int a, int b) {
+        return a - b;
     }
 
-    multiply() {
-
+    int multiply(int a, int b) {
+        return a * b;
     }
 
-    divide() {
-
+    int divide(int a, int b) {
+        return a / b;
     }
 
-    modulo() {
-        
+    int modulo(int a, int b) {
+        return a % b;
     }
 }
 
 public class Math1 {
     public static void main(String[] args) {
-
+        Calculator calc = new Calculator();
+        System.out.println(calc.add(1, 2));
+        System.out.println(calc.subtract(1, 2));
+        System.out.println(calc.multiply(1, 2));
+        System.out.println(calc.divide(1, 2));
+        System.out.println(calc.modulo(1, 2));
     }
 }
