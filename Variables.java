5c93edaa8513f7468b908fdb98c21f4e5474238a
iheartny888-dev <iheartny888@gmail.com>
2025-12-04 14:09:11 +0000

    a

--- Variables.java
+++ Variables.java
@@ -1,3 +1,18 @@
+//two typees- dynamic and static, use camelcase, constants use uppercase
+
+class Student {
+    String studentName; // belongs to individual student
+    static int studentCount = 0; //belongs to the class
+    static final String SCHOOL_NAME = "CSAEA";
+
+    Student(String n) {
+        studentName = n;
+        System.out.println(studentName + " is a student at " + SCHOOL_NAME + ".");
+    }
+}
+
 public class Variables {
-    
+    public static void main(String[] args) {
+        new Student("Alex");
+    }
 }
