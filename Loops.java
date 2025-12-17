f048c1a57b4974c0a4a86da9cc0d5a0955afa7d5
iheartny888-dev <iheartny888@gmail.com>
2025-12-17 15:40:01 +0000

    a

--- Loops.java
+++ Loops.java
@@ -1,8 +1,37 @@
 public class Loops {
     public static void main(String[] args) {
+
+        //break - stops the loop entirely
+        //continue - continues to the next iteration of the loop
+        //return - inside nested loop, ends ALL loops that are occuring, ends the whole function
+
         for (int i = 0; i < 5; i += 2) {
-            for (int j = 0; j < 5; j += 2) {
-                System.out.println(j);
+            if (i == 2) {
+                continue;
+            }
+            System.out.println(i);
+        }
+
+
+        int m = 0;
+        while (m < 5) { 
+            System.out.println(m);
+            
+            m++;
+        }
+
+        boolean isContinuing = true;
+
+        while (isContinuing) {
+            isContinuing = false;
+        }
+
+        for (int i = 0; i < 100; i++) {
+            if (i % 3 == 0) {
+                System.out.print("foo");
+            }
+            if (i % 5 == 0) {
+                System.out.print("bar");
             }
         }
     }
