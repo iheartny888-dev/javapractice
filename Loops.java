f6e0352c51524542a4e97414a45ec1d5b77bbcd3
iheartny888-dev <iheartny888@gmail.com>
2025-12-17 15:23:09 +0000

    a

---
+++ Loops.java
@@ -0,0 +1,9 @@
+public class Loops {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i += 2) {
+            for (int j = 0; j < 5; j += 2) {
+                System.out.println(j);
+            }
+        }
+    }
+}
