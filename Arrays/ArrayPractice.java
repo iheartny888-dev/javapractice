e23642d93626ca3bbbc2f63f17454d424d637f9e
iheartny888-dev <iheartny888@gmail.com>
2026-01-08 15:12:15 +0000

    a

--- Arrays/ArrayPractice.java
+++ Arrays/ArrayPractice.java
@@ -8,5 +8,29 @@ public class ArrayPractice {
             }
         }
         System.out.println(min);
+
+        int target = 6;
+        int[] nums = {0, 2, 4, 6, 8};
+        boolean targetFound = false;
+
+        for (int x = 0; x < nums.length; x++) {
+            if (nums[x] == target) {
+                targetFound = true;
+                System.out.println(targetFound);
+                break;
+            }
+            System.out.println(targetFound);
+        }
+
+        int[] numbers = {0, 0, 0, 1, 0};
+        boolean allSame = true;
+        for (int y = 1; y < numbers.length; y++) {
+            if (numbers[0] != numbers[y]) {
+                allSame = false;
+                break;  
+            }
+                     
+        }
+        System.out.println(allSame);
     }
 }
