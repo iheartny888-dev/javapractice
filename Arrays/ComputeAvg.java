3169c7fa0703e2eb282d43769749511e70036cb3
iheartny888-dev <iheartny888@gmail.com>
2026-01-08 15:41:24 +0000

    a

---
+++ Arrays/ComputeAvg.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ComputeAvg {
+    public static void main(String[] args) {
+        int avg = 0;
+        Scanner score = new Scanner(System.in);
+        int[] scores = {score.nextInt(), score.nextInt(), score.nextInt(), score.nextInt(), score.nextInt()};
+        for (int scr : scores) {
+            avg += scr;
+        }
+        avg = avg / 4;
+        System.out.println(avg);
+    }
+}
