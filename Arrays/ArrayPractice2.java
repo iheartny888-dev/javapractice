a3d8b4c0796c6c95edbe43b38e2ecb4de5dbfa96
iheartny888-dev <iheartny888@gmail.com>
2026-01-14 14:58:19 +0000

    a

--- Arrays/ArrayPractice2.java
+++ Arrays/ArrayPractice2.java
@@ -3,5 +3,14 @@ import java.util.*;
 public class ArrayPractice2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int[][] nums = new int[3][2];
+
+        for (int[] num : nums) {
+            for (int n : num) {
+                System.out.println("Enter a value for the row: " + num + " and column: " + n);
+                nums[0][0] = input.nextInt();
+            }
+        }
+        
     }
 }
