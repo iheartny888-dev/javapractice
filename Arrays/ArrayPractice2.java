c12bb3dc6fd84873a44255fb29859e51d2388eff
iheartny888-dev <iheartny888@gmail.com>
2026-01-14 15:09:03 +0000

    a

--- Arrays/ArrayPractice2.java
+++ Arrays/ArrayPractice2.java
@@ -5,12 +5,14 @@ public class ArrayPractice2 {
         Scanner input = new Scanner(System.in);
         int[][] nums = new int[3][2];
 
-        for (int[] num : nums) {
-            for (int n : num) {
-                System.out.println("Enter a value for the row: " + num + " and column: " + n);
-                nums[0][0] = input.nextInt();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.println("Enter a value for the row: " + i + " and column: " + j);
+                nums[i][j] = input.nextInt();
             }
         }
         input.close();
+
+        System.out.println(Arrays.deepToString(nums).replace("], ", "]\n"));
     }
 }
