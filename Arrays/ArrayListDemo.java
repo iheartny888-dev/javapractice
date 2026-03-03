c7bbbeec5b342a1f9fcfa739e3d39e7fe2e8eb0f
iheartny888-dev <iheartny888@gmail.com>
2026-03-03 15:37:08 +0000

    a

---
+++ Arrays/ArrayListDemo.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>(5);
+        System.out.println("Size of ArrayList: " + names.size());
+        System.out.println("ArrayList: " + names);
+    }
+}
