069cc46d34cd472b6dd7e152f2d422fb800ce8d9
iheartny888-dev <iheartny888@gmail.com>
2026-03-03 15:10:53 +0000

    a

--- WrapperClassesTester.java
+++ WrapperClassesTester.java
@@ -20,4 +20,18 @@ class Wrapper {
         int parsed = Integer.parseInt("25");
         System.out.println("Parsed integer from string: " + parsed);
     }
+
+    public static void demoBoolean() {
+        boolean primitiveBool = true;
+        Boolean myBool = false;
+
+        System.out.println("Primitive boolean: " + primitiveBool);
+        System.out.println("Wrapper Boolean: " + myBool);
+
+        System.out.println("Value of Boolean.TRUE: " + Boolean.TRUE);
+        System.out.println("Value of Boolean.FALSE: " + Boolean.FALSE);
+
+        boolean parsed = Boolean.parseBoolean("true");
+        System.out.println("Parsed boolean from string: " + parsed);
+    }
 }
