f5d0c2810a0991c8935f31563d2472be00cedb67
iheartny888-dev <iheartny888@gmail.com>
2026-03-04 14:59:17 +0000

    a

--- Arrays/ArrayListDemo.java
+++ Arrays/ArrayListDemo.java
@@ -1,95 +1,121 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Random;
+import java.util.Scanner;
 
 class Class {
 
 }
+
 public class ArrayListDemo {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         ArrayList<Integer> list = new ArrayList<Integer>(5);
         System.out.println(list);
         System.out.println(list.size());
 
+        // ArrayList<Integer> list2 = new ArrayList<Double>();
 
-       //  ArrayList<Integer> list2 = new ArrayList<Double>();
+        /// ArrayList<> list3 = new ArrayList<Integer>();
 
-      ///  ArrayList<> list3  = new ArrayList<Integer>(); 
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        System.out.println(list);
+        System.out.println(list.size());
 
+        list.add(1, 5);
+        System.out.println(list);
 
-      list.add(1);
-      list.add(2);
-      list.add(3);
-      list.add(4);
-      list.add(5);
-      System.out.println(list);
-      System.out.println(list.size());
+        System.out.println(list.get(1));
 
+        list.remove(1);
+        System.out.println(list);
 
-      list.add(1, 5);
-      System.out.println(list);
+        list.add(1, 5);
+        list.add(2, 5);
 
+        list.remove(Integer.valueOf(5));
+        System.out.println(list);
 
-      System.out.println(list.get(1));
+        System.out.println("Contains 5: " + list.contains(5));
 
+        Collections.sort(list);
+        Integer value = Collections.binarySearch(list, 4);
+        System.out.println(value);
+        System.out.println(list);
 
-      list.remove(1);
-      System.out.println(list);
+        Integer[] arr = list.toArray(new Integer[2]);
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
 
-     list.add(1, 5);
-     list.add(2, 5);
+        ArrayList<Integer> splitList = new ArrayList<Integer>(list.subList(3, list.size() - 1));
+        System.out.println(splitList);
 
-     list.remove(Integer.valueOf(5));
-     System.out.println(list);
+        list.clear();
+        System.out.println(list);
 
+        System.out.println(list.size());
 
+        // Create an arrayList with 10 random numbers
+        // Remove all odd from that list
 
-     System.out.println("Contains 5: " + list.contains(5));
+        Random rand = new Random();
+        ArrayList<Integer> randomList = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            randomList.add(rand.nextInt(100));
+        }
+        for (int j = 0; j < randomList.size(); j++) {
+            if (randomList.get(j) % 2 != 0) {
+                randomList.remove(j);
+                j--;
+            }
 
+        }
+        System.out.println(randomList);
 
+        // Create a list of grocery items
+        // if the user types in a food print out the index
+        // If not print "Not Found" and add it to the list
+        // continue until you type exit
 
-     Collections.sort(list);
-     Integer value =  Collections.binarySearch(list, 4);
-     System.out.println(value);
-     System.out.println(list);
+        ArrayList<String> groceryList = new ArrayList<String>(Arrays.asList("Milk", "Eggs", "Bread", "Butter"));
+        Scanner scan = new Scanner(System.in);
+        String food = scan.nextLine();
+        if (groceryList.contains(food)) {
+            System.out.println(groceryList.indexOf(food));
+        } else {
+            System.out.println("Not Found");
+            groceryList.add(food);
+        }
+        food = scan.nextLine();
+        scan.close();
 
+        // Make a guest list that allows the user to enter names
+        // if the name exists tell the user that "this person is invited!"
+        // continue until you type exit
 
-    Integer[] arr = list.toArray(new Integer[2]);
-    for (int val : arr) {
-        System.out.print(val + " ");
+        Scanner input = new Scanner(System.in);
+        ArrayList<String> guestList = new ArrayList<String>();
+        guestList.add(input.nextLine());
+        guestList.add(input.nextLine());
+        guestList.add(input.nextLine());
+
+        String name = input.nextLine();
+        if (guestList.contains(name)) {
+            System.out.println("This person is invited!");
+        } else {
+            System.out.println("This person is not invited!");
+        }
+        name = input.nextLine();
+        input.close();
     }
-    System.out.println();
 
-
-    ArrayList<Integer> splitList = new ArrayList<Integer>(list.subList(3,list.size()-1));
-    System.out.println(splitList);
-    
-    list.clear();
-    System.out.println(list);
-
-
-
-
-
-    System.out.println(list.size());
-
-    
-
-    // Create an arrayList with 10 random numbers
-    // Remove all odd from that list
-
-
-    // Make a guest list that allows the user to enter names
-    // if the name exists tell the user that "this person is invited!"
-    // continue until you type exit
-
-
-    // Create a list of grocery items
-    // if the user types in a food print out the index
-    // If not print "Not Found" and add it to the list
-    // continue until you type exit
-}
-    
 }
