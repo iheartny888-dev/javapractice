cacb831426928ea94538a6995e07706694814d6f
iheartny888-dev <iheartny888@gmail.com>
2026-05-08 12:09:30 +0000

    a

--- Recursion.java
+++ Recursion.java
@@ -1,12 +1,21 @@
 public class Recursion {
     public static void main(String[] args) {
     RecursionAlgos r = new RecursionAlgos();
-    r.infinite();
+    r.countDown(5);
     }
 }
 
 class RecursionAlgos {
     public void infinite() {
         infinite();
+    }
+
+    public void countDown(int n) {
+        if (n <= 0) {
+            System.out.println("Hooray!");
+            return;
+        }
+        System.out.println(n);
+        countDown(n - 1);
     }
 }
