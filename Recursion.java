77ed50303ce47f6f3e393ed045d0d26fdfcb311d
iheartny888-dev <iheartny888@gmail.com>
2026-05-08 12:06:29 +0000

    a

---
+++ Recursion.java
@@ -0,0 +1,15 @@
+public class Recursion {
+    public static void main(String[] args) {
+
+    RecursionAlgos r = new RecursionAlgos();
+    r.infinite();
+
+
+    }
+}
+
+class RecursionAlgos {
+    public void infinite() {
+        infinite();
+    }
+}
